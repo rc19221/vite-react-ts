5b9116295ba29d374438ab76487127da0febc43a
RC19221 <jaiatul.parmar@ryan.com>
2025-08-29 16:27:10 +0530

    Finally done

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,17 @@
-import { app } from '@azure/functions';
+import { app } from "@azure/functions";
 
 app.setup({
-    enableHttpStream: true,
+  enableHttpStream: true
 });
 
-app.http('helloworld', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        return {
-            status: 200,
-            body: JSON.stringify({ msg: 'Hello World!' })
-        };      
-    }
-})
+app.http("helloworld", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    return {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ msg: "Hello World!" })
+    };
+  }
+});
